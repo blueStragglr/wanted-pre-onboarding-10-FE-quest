b3ceb4cad8b4d9980e324099c41c2af56a23d4e0
seungmin-dev <seungmin.dev@gmail.com>
2023-09-17 16:28:49 +0900

    Feat: 로그인페이지, API 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header>
+          <ul>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/api">API</Link>
+            </li>
+          </ul>
+        </header>
+      </div>
+      <Outlet />
+    </>
   );
 }
 
---
+++ src/styles/APIStyles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 500px;
+  height: auto;
+  margin: 50px auto;
+  border: 2px solid black;
+  border-radius: 30px;
+`;
+export const Title = styled.h1`
+  text-align: center;
+  padding: 10px 0;
+`;
+export const Item = styled.div`
+  width: 100%;
+  height: 60px;
+  box-sizing: border-box;
+  padding: 0 20px;
+  border-top: 2px solid black;
+  line-height: 60px;
+  font-weight: bold;
+`;
