11a812d73c97858fec3c7ad75ae32eeb9dea5f88
mynameisjisoo <devjisoolee@gmail.com>
2023-09-18 23:40:37 +0900

    project setting

---
+++ login-quest/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <form action="" className="flex">
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
