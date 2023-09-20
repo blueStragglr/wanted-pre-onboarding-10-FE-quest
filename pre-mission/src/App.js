c777b59952239ea6a8bb64ea53f2296a37869bfd
YeonjunJung <duswns12893@gmail.com>
2023-09-20 11:32:44 +0900

    Feat: 메인 페이지 설정 및 라우팅

---
+++ pre-mission/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
