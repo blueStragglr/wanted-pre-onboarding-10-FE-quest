d7f4e5de978ed71261f87f72a8e58336e42ec240
shinhayeong <shy@sqkcloud.com>
2023-09-22 19:31:33 +0900

    Chore : 환경세팅

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { Stack, Button } from '@mui/material';
+import { NextPage } from 'next/types';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <Stack spacing={2} direction='row'>
+          <Button variant='text'>Text</Button>
+          <Button variant='contained'>Contained</Button>
+          <Button variant='outlined'>Outlined</Button>
+        </Stack>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
