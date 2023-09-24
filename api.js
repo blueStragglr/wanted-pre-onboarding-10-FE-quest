4947eff9cc85e6eb82c7a7790dab5390b929980b
Jiyeong <119286806+SPOTBENGALS@users.noreply.github.com>
2023-09-25 06:41:18 +0900

    Add Files

---
+++ api.js
@@ -0,0 +1,27 @@
+const APIBox = document.querySelector(".APIBox");
+const ApiTextBox = document.querySelector(".ApiTextBox");
+const APICallButton = document.querySelector(".APICallButton");
+
+if (ApiTextBox.innerHTML === "") {
+  APIBox.classList.add("hidden");
+}
+
+function showAPI(data) {
+  APIBox.classList.remove("hidden");
+
+  for (let i = 0; i < data.length; i++) {
+    let dataDiv = document.createElement("div");
+    dataDiv.innerHTML = data[i].id + ". " + data[i].name;
+    console.log(dataDiv.innerHTML);
+    dataDiv.setAttribute("class", "DataBox");
+    ApiTextBox.appendChild(dataDiv);
+  }
+}
+
+function APICall() {
+  fetch("https://64f732e69d775408495348ae.mockapi.io/api/v1/authmock")
+    .then((res) => res.json())
+    .then((data) => showAPI(data));
+}
+
+APICallButton.addEventListener("click", APICall);
