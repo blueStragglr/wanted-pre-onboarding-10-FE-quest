ca0324e3e7472bab31935cf5fc1ebbe8fec033e4
jihwan1211 <jhwank1211@gmail.com>
2023-09-24 23:59:56 +0900

    submit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <Link to="/login">로그인 페이지로 가기</Link>
+      <Link to="/login">로그인 페이지로 가기!</Link>
       <br />
-      <Link to="/api">api 호출 페이로 가기</Link>
+      <Link to="/api">api 호출 페이로 가기!</Link>
     </>
   );
 };
