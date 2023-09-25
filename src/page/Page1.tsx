71872e0d592c2bf0f0eeef7c080f771e03ddf5cf
aaminha <dks4857@gmail.com>
2023-09-25 13:55:43 +0900

    ✨ Page1 초안

---
+++ src/page/Page1.tsx
@@ -0,0 +1,18 @@
+const Page1 = () => {
+  return (
+    <main>
+      <form>
+        <label>
+          <span>아이디</span>
+          <input />
+        </label>
+        <label>
+          <span>비밀번호</span>
+          <input />
+        </label>
+      </form>
+    </main>
+  );
+};
+
+export default Page1;
