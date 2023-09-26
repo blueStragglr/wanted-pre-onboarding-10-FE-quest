0643d513fc6a03065465813c6ac2fae02dd8c6ea
aaminha <dks4857@gmail.com>
2023-09-26 18:46:45 +0900

    💄 styled component 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-//import "./App.css";
 import Page1 from "./page/Page1";
+import { GlobalStyle } from "./style/global";
 
 function App() {
-  return <Page1 />;
+  return (
+    <>
+      <GlobalStyle />
+      <Page1 />
+    </>
+  );
 }
 
 export default App;
---
+++ src/style/global.tsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+${reset}
+/* other styles */
+`;
