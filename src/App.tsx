1b1b5975317a03df805173712fcb56ea22726746
aaminha <dks4857@gmail.com>
2023-09-26 19:19:09 +0900

    💄 css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import Page1 from "./page/Page1";
 import { GlobalStyle } from "./style/global";
 
 function App() {
   return (
-    <>
+    <Container>
       <GlobalStyle />
       <Page1 />
-    </>
+    </Container>
   );
 }
 
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+`;
 export default App;
--- src/page/Page1.tsx
+++ src/page/Page1.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 interface Login {
   id: string;
@@ -23,26 +24,83 @@ const Page1 = () => {
   };
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <span>아이디</span>
-        <input
-          placeholder="id"
-          onChange={handleChange}
-          name="id"
-          value={loginData.id}
-        />
-        <span>비밀번호</span>
-        <input
-          placeholder="password"
-          onChange={handleChange}
-          name="password"
-          value={loginData.password}
-        />
+    <MainContainer>
+      <Form onSubmit={handleSubmit}>
+        <div className="form-item">
+          <span>아이디</span>
+          <input
+            placeholder="id"
+            onChange={handleChange}
+            name="id"
+            value={loginData.id}
+          />
+        </div>
+        <div className="form-item">
+          <span>비밀번호</span>
+          <input
+            placeholder="password"
+            onChange={handleChange}
+            name="password"
+            value={loginData.password}
+          />
+        </div>
         <button type="submit">버튼</button>
-      </form>
-    </main>
+      </Form>
+    </MainContainer>
   );
 };
+
+const MainContainer = styled.div`
+  background-color: white;
+  padding: 40px;
+  border-radius: 30px;
+  color: #0d3b66;
+  box-shadow: 2px 4px 8px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  .form-item {
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+    width: 350px;
+
+    span {
+      font-weight: 600;
+      font-size: 1rem;
+    }
+
+    input {
+      background-color: white;
+      padding: 10px 25px;
+      font-size: 1.1rem;
+      border: solid 3px;
+      border-color: #0d3b66;
+      border-radius: 25px;
+
+      &::placeholder {
+        color: gray;
+      }
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  button {
+    background-color: #0d3b66;
+    color: white;
+    padding: 10px 0px;
+    border: none;
+    border-radius: 20px;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+`;
 
 export default Page1;
