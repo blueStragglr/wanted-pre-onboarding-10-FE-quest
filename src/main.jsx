0c1989dbcb63c57bb0180d5a4d1f9ae8bf63fa4d
Jin <jinnkimm7@gmail.com>
2023-09-27 11:47:21 +0900

    feat: 페이지 경로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.jsx'
+import MockPage from './pages/MockPage.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/mock",
+    element: <MockPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
