9f91dc98ad8c5b0432a1b46b7bdb09336536d449
anjxxyi <anjxxyi@gmail.com>
2023-09-29 04:00:41 +0900

    feat: 로그인 페이지 구현

--- src/components/auth/SigninForm.jsx
+++ src/components/auth/SigninForm.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SigninForm = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handlerOnId = (e) => setId(e.target.value);
+  const handlerOnPw = (e) => setPw(e.target.value);
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    if(!id || !pw) alert("Please enter your ID or PW.");
+    console.log('ID : ' + id);
+    console.log('PW : ' + pw);
+  }
+
   return (
     <article>
+      <h2>Sign in</h2>
+      <form id="form" onSubmit={handlerSubmit}>
+        <div className="form-group">
+          <label>ID</label>
+          <input type="text" name="id" value={id} onChange={handlerOnId} placeholder="Enter ID" />
+        </div>
+        <div className="form-group">
+          <label>PW</label>
+          <input type="password" name="password" autoComplete="on" value={pw} onChange={handlerOnPw} placeholder="Enter Password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </article>
   )
 }
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import SigninForm from '../components/auth/signinForm'
 
 const AuthPage = () => {
   return (
     <section className="page-auth">
+      <SigninForm />
     </section>
   )
 }
