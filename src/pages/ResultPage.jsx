a32e12888cf9268fcf163070afac442ce05c1df0
anjxxyi <anjxxyi@gmail.com>
2023-09-29 04:01:44 +0900

    feat: API호출 페이지 구현

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ResultPage = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(
+      "https://64f732e69d775408495348ae.mockapi.io/api/v1/authmock")
+      .then(response => setData(response.data)
+      )
+  }, []);
+
   return (
     <section className="page-result">
+      <h2>Data Result</h2>
+      <ul>
+        {data.map((value, index) => {
+          return (
+            <li key={index}>
+              <span>ID : {value.id}</span>
+              <span>Name : {value.name}</span>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
