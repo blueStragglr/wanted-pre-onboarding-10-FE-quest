2302a7ffec3af4c6d4adbac5857492989d05d3bd
seunghyunkim <martin.kim@legendaries.team>
2023-09-28 17:41:54 +0900

    사전 과제 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './App.css';
+import { useRouter } from './useRouter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [formState, setFormState] = useState({ id: '', pw: '' });
+  const router = useRouter();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { id, pw } = formState;
+
+    if (!id || !pw) {
+      return alert('아이디 또는 패스워드를 입력해주세요.');
+    }
+    console.log('id : ', id);
+    console.log('pw : ', pw);
+
+    router.push('/contents');
+  };
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>, key: string) => {
+    setFormState((prev) => ({ ...prev, [key]: event.target.value }));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <article>
+      <p>👉 Login 👈</p>
+      <form id="form" method="get" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="id">ID </label>
+          <input
+            id="user-id"
+            name="id"
+            placeholder="id를 입력해주세요."
+            value={formState.id}
+            onChange={(e) => onChange(e, 'id')}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="pw">PW </label>
+          <input
+            id="user-pw"
+            name="pw"
+            placeholder="pw를 입력해주세요."
+            value={formState.pw}
+            onChange={(e) => onChange(e, 'pw')}
+          />
+        </div>
+        <button type="submit" className="submit-btn">
+          로그인
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </article>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import Router from './components/Router';
+import Routes from './components/Routes';
+import Route from './components/Route';
+import Contents from './components/Contents';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" component={<App />} />
+        <Route path="/contents" component={<Contents />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
