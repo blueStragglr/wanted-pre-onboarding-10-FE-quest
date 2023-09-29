1963ba0f4d1bb95143ed041a498d0cd5740ca59c
goeun-dev <goeunlee.me@gmail.com>
2023-09-29 15:34:09 +0900

    add: PAGE2 Mock API 호출 결과 출력 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './components/Login'
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import MockAPIResult from "./components/MockAPIResult";
+import Link from "@mui/material/Link";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultTheme = createTheme();
 
   return (
-    <>
-      <Login />
-    </>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        sx={{ border: 1, borderRadius: 1, margin: 2, padding: 2 }}
+      >
+        <Link underline="hover" color="inherit" href="/">
+          Login
+        </Link>
+        <Link underline="hover" color="inherit" href="/mock-api">
+          Mock API
+        </Link>
+      </Breadcrumbs>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/mock-api" element={<MockAPIResult />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const Login = () => {
+  const [loginID, setLoginID] = useState<string>("");
+  const [loginPW, setLoginPW] = useState<string>("");
 
-    const [loginID, setLoginID] = useState<string>('');
-    const [loginPW, setLoginPW] = useState<string>('');
+  const handleChangeID = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginID(e.target.value);
+  };
 
-    const handleChangeID = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLoginID(e.target.value);
-    }
+  const handleChangePW = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginPW(e.target.value);
+  };
 
-    const handleChangePW = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLoginPW(e.target.value);
-    }
+  const handleClickBtn = () => {
+    console.log("ID: ", loginID, ", PW: ", loginPW);
+  };
 
-    const handleClickBtn = () => {
-        console.log('ID: ', loginID, 'PW: ', loginPW)
-    }
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="ID"
+              autoFocus
+              onChange={handleChangeID}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="PW"
+              type="password"
+              onChange={handleChangePW}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleClickBtn}
+            >
+              출력
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
 
-    return (
-        <div className="login">
-            <div>
-                <label>아이디</label>
-                <input onChange={handleChangeID} />
-            </div>
-            <div>
-                <label>비밀번호</label>
-                <input type='password' onChange={handleChangePW} />
-            </div>
-            <button onClick={handleClickBtn}>출력</button>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
