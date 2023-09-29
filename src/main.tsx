1715bb84eadf005993534d60d418fca36e8613cd
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-29 14:47:27 +0900

    feat: provide theme color

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import GlobalStyle from './style/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/style/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    primary: '#6a2c70', // 메인 보라색
+    secondary: '#a56cc1', // 보조 보라색
+    textPrimary: '#000000', // 주 텍스트 컬러
+    textSecondary: '#f9cffc', // 보조 텍스트 컬러
+  },
+};
+
+export default theme;
