cb49bccdac6dd967fd301d2990cc7e27b035eee2
Jin <jinnkimm7@gmail.com>
2023-09-29 21:45:50 +0900

    Fix: mock 페이지 경로 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <Link to="/login">로그인 페이지</Link>
-      <Link to="/login">Mock API 페이지</Link>
+      <Link to="/mock">Mock API 페이지</Link>
     </div>
   )
 }
