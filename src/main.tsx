35a082ea465ebdd7d8edf6f6d060f3a460fb5c7c
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-29 14:06:27 +0900

    feat: apply reset-css

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import GlobalStyle from './style/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
