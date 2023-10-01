6d9a847c9c71afdf5de5a51a4e383d084100d789
hyeongjun <whow1101@naver.com>
2023-10-01 17:38:47 +0900

    chore: react-query 설치 및 셋업

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "@Pages/router";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
