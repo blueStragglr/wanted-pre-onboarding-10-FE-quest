f7e66dc41ac514a963212210a8cd7e5fb271ea6c
sowon_m1 <bsw2377@gmail.com>
2023-10-03 02:09:20 +0900

    [feat] add login

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,54 @@
+import { useState, useEffect } from "react";
+
+import classes from "./Menu.module.css";
+
+interface menuType {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  iceOnly: boolean;
+}
+
 export default function Menu() {
+  const [menu, setMenu] = useState<menuType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("https://651a8049340309952f0d685d.mockapi.io/menu")
+      
+      if (!response.ok) {
+        throw new Error("Could not fetch data");
+      }
+      const data = await response.json();
+      setMenu(data);
+    } catch(error) {
+      console.error(error)
+    }
+
+    setIsLoading(false);
+  }
+
   return (
-    <div>Menu</div>
+    <div>
+      <div>Menu </div>
+      <div>{isLoading ? "loading..." : "test"}</div>
+      <ul className={classes.menuContainer}>
+        {menu.map((el) => 
+          <li key={el.id} className={classes.menubox}>
+            <div>
+              <div>{}</div>
+            </div>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,34 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 
+import classes from "./Login.module.css";
 
 export default function Login() {
-  return (
-    <form className="">
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = idRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log("id: ", id, " | ", "password: ", password);
+  }
+
+  return (
+    <form className={classes.form} onSubmit={onSubmit}>
+      <h2>Login</h2>
+      <div className={classes.inputArea}>
+        <input id="id" type="text" ref={idRef} placeholder="Id" />
+        <input id="password" type="password" ref={passwordRef} placeholder="Password" />
+      </div>
+      <div className={classes.buttonArea}>
+        <button>Log in</button>
+      </div>
+      <div className={classes.bottomArea}>
+        <Link to={"/"}>Home으로 돌아가기 &rarr;</Link>
+      </div>
     </form>
   )
 }
