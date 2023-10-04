5ca5584b38e92b0778095f51628a19cdd8ce6127
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-10-04 23:02:40 +0900

    add: userList 분리

--- userList.js
+++ userList.js
@@ -1,10 +1,14 @@
-async function getData() {
-  try {
-    const response = await fetch('https://64f732e69d775408495348ae.mockapi.io/api/v1/authmock')
-    const result = await response.json();
-    console.log('result:::',result);
-  } catch(error) {
-    console.log(error)
-  }
-}
-const data = fetch('https://64f732e69d775408495348ae.mockapi.io/api/v1/authmock').then((res) => res.json()).catch((err) => console.log(err));
+const result = [];
+
+function getData() {
+    fetch('https://64f732e69d775408495348ae.mockapi.io/api/v1/authmock')
+        .then((res) => res.json())
+        .then(data => {
+            const name = document.createElement("div");
+            name.textContent = data.name;
+
+            const userInfo = document.querySelector('#userList');
+            userInfo.appendChild(name)
+        })
+        .catch((err) => console.log(err));
+} 
