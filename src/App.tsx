10a18467ed25da9344d691395c251154d54720d2
ho04029 <ho04029@naver.com>
2023-10-04 16:29:12 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import MockApi from "./pages/MockApi";
+import Header from "./components/Header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Header>
+        <Login />
+      </Header>
+    ),
+  },
+  {
+    path: "/mock",
+    element: (
+      <Header>
+        <MockApi />
+      </Header>
+    ),
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
