f1f1373dc53733fad1cdb0e9abe8e2ccf7b7412e
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-10-06 15:18:55 +0900

    init: set

--- index.js
+++
@@ -1,9 +0,0 @@
-function onChangeId() {
-    const userId = document.getElementById('user_id').value;
-    console.log('userId::', userId)
-}
-
-function onChangePw() {
-    const userPw = document.getElementById('user_pw').value;
-    console.log('userPw::', userPw)
-}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import './style.css';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+};
