2c422f59b949a24cf42e85e2781eecc805d6dc6f
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-11 16:58:48 +0900

    feat: qhrtmq

--- vite-main/src/App.tsx
+++ vite-main/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Routes,Route}from "react-router-dom"
-import Header from './component/Header';
-import Page1 from './page/Page1';
-import Page2 from './page/Page2';
+import {createBrowserRouter,RouterProvider}from "react-router-dom"
+import LoginWithMockAPI from "./pages/LoginWithMockAPI";
+const router =createBrowserRouter([{
+ path : "/",
+ element :<LoginWithMockAPI/>,
+
+}])
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Page1/>}></Route>
-        <Route path="/page/2" element={<Page2/>}></Route>
-      </Routes>
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- vite-main/src/main.tsx
+++ vite-main/src/main.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import {worker} from "./mocks/worker";
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
-// worker.start();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+
     <App />
-    </BrowserRouter>
+
   </React.StrictMode>
 )
